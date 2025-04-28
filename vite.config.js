e9fd410e281238aabecc943b0ce7752727879e52
pranayburra <125851398+pranayburra@users.noreply.github.com>
2025-04-28 17:27:55 +0530

    Fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
